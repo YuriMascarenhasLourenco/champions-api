661cc553a309e200510c0b649f805be752557fe8
Yuri Lourenço <lourenco99yuri@gmail.com>
2025-02-23 01:28:16 -0300

    conecting typeorm

--- src/clubs/clubs.module.ts
+++ src/clubs/clubs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClubsService } from './service/clubs.service';
 import { ClubsController } from './controller/clubs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Club } from './entities/clubs.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Club])],
   controllers: [ClubsController],
   providers: [ClubsService],
 })
--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './services/players.service';
 import { PlayersController } from './controllers/players.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Player } from './entities/player.entity';
+import { Abilities } from './entities/abilities.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Player,Abilities])],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
